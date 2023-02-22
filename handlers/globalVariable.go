457e836a42c550857028a3251f86facea9bf68f0
tsm <693555980@qq.com>
2023-02-22 10:22:59 +0800

    add lock for request zip file

--- handlers/globalVariable.go
+++ handlers/globalVariable.go
@@ -1,9 +1,14 @@
 package handlers
 
+import "sync"
+
 var ClientId string = "1"
 var CUDA_DEVICE string
 var CenterServerAddr string = "http://127.0.0.1:23333"
 var HostAddr string
+
+var requestFile map[string]*sync.Mutex
+var requestFileLock sync.RWMutex
 
 // var RenderFinish chan string
 // var RelocaliseFinish chan relocaliseInfo //two scene
